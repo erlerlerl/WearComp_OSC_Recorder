841d6274942cf6c1b9b3fdcc25e73e428f561260
erlerlerl <r.rodrigueserl@gmail.com>
2026-09-14 15:30:49 +0200

    liblo linked successfully, created testserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <lo/lo.h>
 
 auto main(int argc, char *argv[]) -> int {
 
-    std::cout << "hello recorder";
+    lo_server server = lo_server_new("9000", nullptr);
+
+    if (!server) {
+        std::cerr << "Could not create OSC server \n";
+        return 1;
+    }
+
+    std::cout << "liblo is linked successfully.\n";
+    std::cout << "OSC server created on port 9000.\n";
+
+    lo_server_free(server);
 
     return 0;
 }
