02c0316903813ebbefc4eaf27bd2ab7ad4c8897c
erlerlerl <r.rodrigueserl@gmail.com>
2026-09-14 15:38:33 +0200

    add simple listening and logging of incoming OSC

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <lo/lo.h>
 
+int oscHandler (const char* path, const char* types, lo_arg** argv, int argc, lo_message msg, void* user_data
+){
+
+    std::cout << "OSC received " << path;
+
+    for (int i = 0; i < argc; i++) {
+        if (types[i] == 'f') {
+            std::cout << " " << argv[i]->f;
+        } else if (types[i] == 'i') {
+            std::cout << " " << argv[i]->i;
+        } else if (types[i] == 's') {
+            std::cout << " " << &argv[i]->s;
+        }
+
+    }
+    std::cout << std::endl;
+    return 0;
+
+}
+
 auto main(int argc, char *argv[]) -> int {
 
     lo_server server = lo_server_new("9000", nullptr);
@@ -10,8 +30,17 @@ auto main(int argc, char *argv[]) -> int {
         return 1;
     }
 
-    std::cout << "liblo is linked successfully.\n";
-    std::cout << "OSC server created on port 9000.\n";
+    lo_server_add_method(server,  // server
+                         nullptr, // alle OSC Paths
+                         nullptr, // ally type tags
+                         oscHandler, // Methode
+                         nullptr);
+
+    std::cout << "OSC server listening on port 9000...\n";
+
+    while (true) {
+        lo_server_recv(server);
+    }
 
     lo_server_free(server);
 
